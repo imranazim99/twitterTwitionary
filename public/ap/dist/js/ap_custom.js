878e4525ed510e8d808ca9ca6dba50f3f385542e
Imran Azim <imranazim99@gmail.com>
2021-02-22 16:29:39 +0500

    function number to string added

--- public/ap/dist/js/ap_custom.js
+++ public/ap/dist/js/ap_custom.js
@@ -108,3 +108,24 @@ $(document).ready(function () {
 		});
 	});
 });
+
+// convert numbers to string of k,m,g,t, etc
+function numToLetter(num, digits = 2) {
+	var si = [
+		{ value: 1, symbol: "" },
+		{ value: 1E3, symbol: "k" },
+		{ value: 1E6, symbol: "M" },
+		{ value: 1E9, symbol: "G" },
+		{ value: 1E12, symbol: "T" },
+		{ value: 1E15, symbol: "P" },
+		{ value: 1E18, symbol: "E" }
+	];
+	var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+	var i;
+	for (i = si.length - 1; i > 0; i--) {
+		if (Math.abs(num) >= si[i].value) {
+		break;
+		}
+	}
+	return (Math.abs(num) / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
